3196c241bf506cb01ed2d7c029f97509e4880514
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-09-25 06:04:45 +0200

    Modifying security - cors config

--- examserver/src/main/java/com/exam/examserver/config/MySecurityConfig.java
+++ examserver/src/main/java/com/exam/examserver/config/MySecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -23,9 +24,9 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailsService;
 	@Autowired
-	private JwtAuthenticationEntryPoint unAuthorizedHandler;
-	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	@Autowired
+	private CorsConfigurationSource corsConfigurationSource;
 	
 	@Bean
 	BCryptPasswordEncoder passwordEncoder() {
@@ -51,8 +52,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
         http
             .csrf(csrf -> csrf
                     .disable())
-            .cors(cors -> cors
-                    .disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .authorizeRequests(requests -> requests
                     .antMatchers("/generate-token", "/user/").permitAll()
                     .antMatchers(HttpMethod.OPTIONS).permitAll()
@@ -61,7 +61,5 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 	}
-
-	
     
 }
